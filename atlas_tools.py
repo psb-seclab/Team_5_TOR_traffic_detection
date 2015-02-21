17d2c87b3315028a1996f8e4faa75438c4c035f0
Dylan Taylor <dylan.m.taylor92@gmail.com>
2015-02-20 20:59:40 -0500

    note to self: learn python

--- atlas_tools.py
+++ atlas_tools.py
@@ -2,4 +2,4 @@ import requests
 
 def checkRelay(ip_addr,UTCtimestamp):
 	r = requests.get("https://exonerator.torproject.org/?targetaddr=&targetPort=&ip="+ip_addr+"&timestamp="+ UTCtimestamp + "#relay")
-	return !(r.content.contains("NEGATIVE"))
+	return (not (r.content.contains("NEGATIVE")))
